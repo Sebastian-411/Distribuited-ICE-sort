e95c5562f8bcf095aba50efddeeb1a79218467e3
elkofix <holasoygermamwow@gmail.com>
2024-04-29 10:16:08 -0500

    upload more types methods

--- server/src/main/java/SorterI.java
+++ server/src/main/java/SorterI.java
@@ -36,4 +36,67 @@ public class SorterI implements Sorter{
         }
         return result;
     }
+
+    @Override
+    public String[] divideAndSortS(String[] arr, Current current) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+        int mid = arr.length / 2;
+        String[] left = divideAndSortS(Arrays.copyOfRange(arr, 0, mid), current);
+        String[] right = divideAndSortS(Arrays.copyOfRange(arr, mid, arr.length), current);
+        return mergeS(left, right, current);
+    }
+    
+    @Override
+    public String[] mergeS(String[] arr1, String[] arr2, Current current) {
+        String[] result = new String[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i].compareTo(arr2[j]) < 0) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+        while (i < arr1.length) {
+            result[k++] = arr1[i++];
+        }
+        while (j < arr2.length) {
+            result[k++] = arr2[j++];
+        }
+        return result;
+    }
+    
+    @Override
+    public double[] divideAndSortD(double[] arr, Current current) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+        int mid = arr.length / 2;
+        double[] left = divideAndSortD(Arrays.copyOfRange(arr, 0, mid), current);
+        double[] right = divideAndSortD(Arrays.copyOfRange(arr, mid, arr.length), current);
+        return mergeD(left, right, current);
+    }
+    
+    @Override
+    public double[] mergeD(double[] arr1, double[] arr2, Current current) {
+        double[] result = new double[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+        while (i < arr1.length) {
+            result[k++] = arr1[i++];
+        }
+        while (j < arr2.length) {
+            result[k++] = arr2[j++];
+        }
+        return result;
+    }
+    
 }
